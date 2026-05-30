8bea085fbbfc24c8059eae7669ec77ddd6f6499e
son-coder <vison2999@gmail.com>
2026-05-30 07:34:46 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import path from 'path'; // Thay đổi cách import
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        menu: path.resolve(__dirname, "menu.html"),
+        album: path.resolve(__dirname, "album.html"),
+        cake: path.resolve(__dirname, "cake.html"),
+        universe: path.resolve(__dirname, "universe.html"),
+      },
+    },
+  },
+});
